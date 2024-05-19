7d84b71cd38e7fe71f85ca0a98edca095407624e
Ramdan <dadanisme@upi.edu>
2024-05-20 02:00:14 +0700

    change initial dashboard

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,16 +1,43 @@
 "use client";
 
-import { auth } from "@/services/firebase";
-import Image from "next/image";
-import { Button } from "react-daisyui";
+import { auth, db } from "@/services/firebase";
+import { collection, collectionGroup } from "firebase/firestore";
 import { useSignOut } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+import { Card as RDCard } from "react-daisyui";
+import { useMemo } from "react";
 
 export default function Home() {
   const [signOut, loading, error] = useSignOut(auth);
+  const [sarana] = useCollection(collectionGroup(db, "saranaRuangan"));
+  const [ruangan] = useCollection(collection(db, "ruangan"));
+  const [kategori] = useCollection(collection(db, "kategori"));
+
+  const totalSarana = useMemo(
+    () => sarana?.docs.reduce((acc, s) => acc + s.data().quantity, 0),
+    [sarana]
+  );
 
   return (
-    <div>
-      <Button onClick={signOut}>Sign Out</Button>
+    <div className="grid md:grid-cols-3 gap-4">
+      <Card title="Total Sarana" count={totalSarana ?? 0} />
+      <Card title="Total Ruangan" count={ruangan?.size ?? 0} />
+      <Card title="Total Kategori" count={kategori?.size ?? 0} />
     </div>
+  );
+}
+
+interface CardProps {
+  title: string;
+  count: number;
+}
+
+function Card({ title, count }: CardProps) {
+  return (
+    <RDCard className="p-4 shadow-md">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <p>{count}</p>
+    </RDCard>
   );
 }
